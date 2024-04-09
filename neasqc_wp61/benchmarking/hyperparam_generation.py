c0eb421d94bb82a9a7afe6c048607808c408ea21
Ryk <47638463+ryk-wolf@users.noreply.github.com>
2024-04-09 15:57:57 +0100

    Update hyperparam_generation.py

--- neasqc_wp61/benchmarking/hyperparam_generation.py
+++ neasqc_wp61/benchmarking/hyperparam_generation.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 data = {
-    'model_type': [None, None],
-    'embedding_type': [None, None],
-    'dimensionality_reduction_technique': [None, None],
-    'ansatz': [None, None],
-    'number_of_layers': [None, None],
-    'number_of_qubits': [None, None],
-    'qubit_initialisation_technique': [None, None],
-    'dimensionality_reduction_technique': [None, None],
-    'optimizer': [None, None],
-    'optimizer_lr': [None, None],
-    'optimizer_number_of_epochs': [None, None],
-    'optimizer_parameters': [None, None],
+    'model_type': ["alpha3"], #Let's not forget Alpha4 later on
+    'embedding_type': ["BERT"], #which ones???
+    'dimensionality_reduction_technique': ["PCA", "ICA", "TSVD", "UMAP", "TSNE"],
+    'ansatz': ["Sim14", "Sim15", "StronglyEntangling"],
+    'number_of_layers': [i for i in range(1, 3+1)],
+    'number_of_qubits': [i for i in range(2, 16+1, 2)],
+    'qubit_initialisation_technique': ["constant", "normal_centered_pi", "uniform_between_0_2pi"], #the names must be checked to match actual names
+    'optimizer': ["Adam", "SPSA", "SGD", "RMSProp", "Cobyla", "Nelder-Mead", "Adagrad"],
+    'optimizer_lr': [0.0001 * (10 ** i) for i in range(5)],
+    'optimizer_parameters': [None], #Not sure what we want here
 }
+
+number_of_epochs = 20 #Fixed for grid search
 
 df = pd.DataFrame(data)
 
@@ -28,4 +28,4 @@ for model in df['model_type']:
                                 for lr in df['optimizer_lr']:
                                     for nb_epochs in df['optimizer_number_of_epochs']:
                                         for opti_params in df['optimizer_parameters']:
-                                            #call model
+                                            print(f"Model: {model} \t embedding : {embedding} \t dimensionality reduction : {dim_reduc} \t ansatz : {ansatz}")
