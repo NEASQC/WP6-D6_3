4e4ff049cff312f1191a6bd8c1765d6c7670355a
Ryk <47638463+ryk-wolf@users.noreply.github.com>
2024-04-12 16:32:18 +0100

    hyper-parameter generation for preliminary grid search

--- neasqc_wp61/benchmarking/hyperparam_generation_PRELIM.py
+++ neasqc_wp61/benchmarking/hyperparam_generation_PRELIM.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import itertools
 
-model_types = ["alpha3"] #Let's not forget Alpha4 later on
+model_types = ["alpha3"]
 embedding_type = ["BERT", "ember"]
-dimensionality_reduction_technique = ["PCA", "ICA", "TSVD", "UMAP", "TSNE"] #add the MLP later on?
+dimensionality_reduction_technique = ["ICA", "UMAP", "TSNE"]
 ansatz = ["Sim14", "Sim15", "StronglyEntangling"]
-number_of_layers = [i for i in range(1, 3+1)]
-number_of_qubits = [i for i in range(2, 16+1, 2)]
+number_of_layers = [2]
+number_of_qubits = [8]
 qubit_initialisation_technique = ["norm", "rescaled_unif"]
-optimizer = ["Adadelta", "Adagrad", "Adam", "AdamW", "SPSA", "SGD", "RMSProp", "Cobyla", "Nelder-Mead", ""]
-optimizer_lr = [0.0001, 0.001, 0.01, 0.1] #1
+optimizer = ["Adam", "RMSProp"] 
+optimizer_lr = [0.001] 
 mlp_init = ["unif", "norm"]
 
 combinations = list(itertools.product(model_types, embedding_type, dimensionality_reduction_technique, ansatz, number_of_layers, number_of_qubits, qubit_initialisation_technique, optimizer, optimizer_lr, mlp_init))
 
+unique_identifiers = [f"{i+1:05d}" for i in range(len(combinations))]
+
 df = pd.DataFrame(combinations, columns=['models', 'embeddings', 'dimensionality_reduction_technique', 'ansatz', 'nb_layers', 'nb_qubits', 'qubit_init_tecchnique', 'optimizer', 'optimizer_lr', 'mlp_init' ])
 
-print(df)
+df.insert(0, 'unique_id', unique_identifiers)
+
+df.to_csv('exp_hyperparameters_PRELIM.tsv', sep='\t', index=False)
+
+#print(df)
