f3b2b5f703c8f2554bfd546755819248d063bee4
pablo-lauret <pablo.lauret@yahoo.es>
2024-04-19 16:39:26 +0100

    Adds updates to alpha 3/4 pipeline (still WIP).

--- neasqc_wp61/data/data_processing/use_alpha_3_4.py
+++ neasqc_wp61/data/data_processing/use_alpha_3_4.py
@@ -7,7 +7,6 @@ models.
 """
 
 import argparse
-import json
 import numpy as np
 import os
 import pandas as pd
@@ -20,45 +19,43 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path + "/../../models/quantum/")
 sys.path.append(current_path + "/../../models/quantum/alpha_3_4/")
 
-import embedder as emb
 import dim_reduction as dimred
 import circuit as circ
 
 from alpha_3_4 import Alpha3, Alpha4
-from save_json_output import JsonOutputer
-
 
 parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-id",
+    "--exp_id",
+    help="Experiment ID for the hyperparameter configuration.",
+    type=int,
+)
 
 # Model-related arguments
 
 parser.add_argument(
     "-m",
     "--model",
-    help="Choice between alpha_3 and alpha_4 model",
+    help="Choice between alpha_3 and alpha_4 model.",
     type=str,
+    choices=['alpha_3', 'alpha_4']
     default="alpha_3",
 )
 parser.add_argument(
-    "-tr",
-    "--train",
-    help="Path to the training dataset",
+    "-dat",
+    "--dataset",
+    help="Path to the full dataset.",
     type=str,
-    default="./../datasets/toy_datasets/multiclass_toy_train_sentence_bert.csv",  # Change?
 )
 parser.add_argument(
-    "-val",
-    "--validation",
-    help="Path to the validation dataset",
-    type=str,
-    default="./../datasets/toy_datasets/multiclass_toy_validation_sentence_bert.csv",  # Change?
-)
-parser.add_argument(
-    "-te",
-    "--test",
-    help="Path to the test dataset",
-    type=str,
-    default="./../datasets/toy_datasets/multiclass_toy_test_sentence_bert.csv",  # Change?
+    "-sp",
+    "--split_id",
+    help="Split ID - indicates what split will be used as validation data."
+         "The split with split ID + 1 will be used as test data.",
+    type=int,
+    default=0,
 )
 parser.add_argument(
     "-nc",
@@ -74,25 +71,11 @@ parser.add_argument(
     default=150298,
 )
 parser.add_argument(
-    "-i",
-    "--iterations",
+    "-e",
+    "--epochs",
     help="Number of iterations/epochs",
     type=int,
     default=150,
-)
-parser.add_argument(
-    "-r",
-    "--runs",
-    help="Number of runs of the model",
-    type=int,
-    default=1,
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    help="Path of directory to store model output",
-    type=str,
-    default="./../../benchmarking/results/raw/",
 )
 
 # Optimiser arguments
@@ -122,17 +105,21 @@ parser.add_argument(
 # Arguments for data vectorisation
 
 parser.add_argument(
-    "-e",
+    "-em",
     "--embedder",
     help="Choice of embedder",
     type=str,
     default="Bert",
 )
+# In current pipeline we pass an already vectorised dataset 
+# as argument, so this argument is for book-keeping
+
 parser.add_argument(
     "-dr",
     "--dim_reduction",
     help="Choice of dimensionality reduction mechanism",
     type=str,
+    choices=['PCA', 'ICA', 'TSVD', 'UMAP'],
     default="PCA",
 )
 
@@ -143,6 +130,7 @@ parser.add_argument(
     "--circuit",
     help="Choice of quantum circuit architecture",
     type=str,
+    choices=['Sim14', 'Sim15', 'StronglyEntanglingAnsatz'],
     default="Sim14",
 )
 parser.add_argument(
@@ -160,16 +148,21 @@ parser.add_argument(
     default=1,
 )
 parser.add_argument(
-    "-qi",
-    "--qubit_init",
-    help="",  # Need clarification here
-    type=str,
-)
-parser.add_argument(
     "-res",
     "--data_rescaling",
-    help="",  # Need clarification
+    help="Function to apply to rescale the inputs that will be encoded \
+        in the first wall of the quantum circuit.", 
     type=str,
+    default=None,
+    choices=['None', 'rescaled_unif', 'unif', 'norm'],
+)
+parser.add_argument(
+    "-ci",
+    "--circuit_init",
+    help="Function to be used to initialise circuit optimisable parameters.",  
+    type=str,
+    default=None,
+    choices=['None', 'rescaled_unif', 'unif', 'norm'],
 )
 
 # Arguments for MLP layer (Alpha 3 only)
@@ -177,18 +170,26 @@ parser.add_argument(
 parser.add_argument(
     "-mi",
     "--mlp_init",
-    help="Distribution to initialise the post-processing layer optimisable parameters",
+    help="Distribution to initialise the post-processing layer optimisable parameters.",
     type=str,
     default=None,
+    choices=['None', 'rescaled_unif', 'unif', 'norm'],
 )
 
 args = parser.parse_args()
 
 
-def main(args):
+def main(args) -> dict:
     """
     Main
+
+    Parameters
+    ----------
+
+    Returns
+    -------
     """
+
     # Set optimiser
     lr = args.learning_rate
 
@@ -217,35 +218,21 @@ def main(args):
     else:
         raise ValueError("Unrecognised torch optimiser given.")
 
-    """
-    # Set embedder
-    embedder_arg = args.embedder.casefold()
-    if embedder_arg == "bert":
-        embedder = emb.Bert()
-    elif embedder_arg == "fasttext":
-        embedder = emb.FastText()
-    elif embedder_arg == "ember":
-        embedder = emb.Ember()
-    else:
-        raise ValueError("Unrecognised embedder type given.")
-    """
-
     # Set dimensionality reduction techninique
-    dimred_arg = args.dim_reduction.casefold()
-    if dimred_arg == "pca":
-        dim_red = dimred.PCA()
-    elif dimred_arg == "ica":
-        dim_red = dimred.ICA()
-    elif dimred_arg == "tsvd":
-        dim_red = dimred.TSVD()
-    elif dimred_arg == "umap":
-        dim_red = dimred.UMAP()
-    elif dimred_arg == "tsne":
-        dim_red = dimred.TSNE()
+    dimred_arg = args.dim_reduction
+    dim_out = args.n_qubits
+    if dimred_arg == "PCA":
+        dim_red = dimred.PCA(dim_out)
+    elif dimred_arg == "ICA":
+        dim_red = dimred.ICA(dim_out)
+    elif dimred_arg == "TSVD":
+        dim_red = dimred.TSVD(dim_out)
+    elif dimred_arg == "UMAP":
+        dim_red = dimred.UMAP(dim_out)
     else:
-        raise ValueError("Unrecognised embedder was given.")
+        raise ValueError("Unrecognised dimensionality reduction technique was given.")
 
-    # et quantum circuit
+    # Set quantum circuit
     if args.circuit == "Sim14":
         ansatz = circ.Sim14()
     elif args.circuit == "Sim15":
@@ -258,54 +245,55 @@ def main(args):
     # Set the rescaling init functions for the circuit inputs,
     # circuit parameters, and MLP parameters
 
-    param_init_functions = [("small", torch.nn.init.uniform_(a=0, b=2*np.pi)), ("unif", torch.nn.init.uniform_(a=0, b=2*np.pi)), ("norm", torch.nn.init.normal_(mean=np.pi, std=np.sqrt(np.pi)))]
+    param_init_functions = [("rescaled_unif", torch.nn.init.uniform_(a=0, b=2*np.pi)), ("unif", torch.nn.init.uniform_(a=0, b=2*np.pi)), ("norm", torch.nn.init.normal_(mean=np.pi, std=np.sqrt(np.pi)))]
 
-    for param_init_function in param_init_functions
-        if args.qubit_init[0] == param_init_function[0]:
-            qubit_init = param_init_functions[1]
-        elif args.qubit_init[0] == "const":
-            qubit_init = torch.nn.init.constant_(val=args.qubit_init[1])
+    for param_init_function in param_init_functions:
+        if args.circuit_init[0] == param_init_function[0]:
+            circuit_init = param_init_functions[1]
         else:
             raise ValueError("Unrecognised qubit init function.")
         
-    for param_init_function in param_init_functions
+    for param_init_function in param_init_functions:
         if args.data_rescaling[0] == param_init_function[0]:
             data_rescaling = param_init_functions[1]
-        elif args.data_rescaling[0] == "const":
-            data_rescaling = torch.nn.init.constant_(val=args.data_rescaling[1])
         else:
             raise ValueError("Unrecognised data rescaling function.")
         
     for param_init_function in param_init_functions:
         if args.mlp_init[0] == param_init_function[0]:
             mlp_init = param_init_functions[1]
-        elif args.mlp_init[0] == "const":
-            mlp_init = torch.nn.init.constant_(val=args.mlp_init[1])
         else:
             raise ValueError("Unrecognised MLP init function.")
 
-    # Initialise objects (ths includes embedder, dim red, etc.)
-
-    #Prepare data
+    # Prepare data
 
     sentence_vectors = []
     labels = []
-    data_paths = [args.train, args.val, args.test]
-    for path in data_paths:
-        if args.embedder not in path.split("_"):
+    dataset_path = args.dataset
+    split_id = args.split_id
+
+    split_data_path = dataset_path.split('_')
+    split_data_path = [item.split('.') for item in split_data_path]
+
+    if args.embedder not in split_data_path:
             raise ValueError("The dataset provided does not correspond"
                              " to the choice of embedder.")
-    train = pd.read_csv(args.train, delimiter='\t')
-    val = pd.read_csv(args.val, delimiter="\t")
-    test = pd.read_csv(args.test, delimiter="\t")
-    for df in [train, val, test]:
-        classes = df["class"].to_numpy()
+
+    dataset = pd.read_pickle(dataset_path)
+    dim_red.fit(data_to_fit=dataset)
+
+    # Split train and val
+    val = dataset[dataset['split'] == split_id]
+    train = dataset[dataset['split'] != split_id]
+
+    for df in [train, val]:
+        classes = df["class"].astype(int).tolist()
         labels.append(classes)
-        dim_red(dataset=df).reduce_dimension(dim_out=args.n_qubits)
-        reduced_embeddings = dim_red.dataset["reduced_sentence_embedding"].to_numpy()
+        reduced_dataset = dim_red.reduce_dimension(data_to_reduce=df)
+        reduced_embeddings = reduced_dataset["reduced_sentence_vector"].tolist()
         sentence_vectors.append(reduced_embeddings)
     
-    #Initialise model with all of its attributes
+    # Initialise model with all of its attributes
     if args.model == "alpha_3":
         circuit = ansatz(
             n_qubits=args.n_qubits,
@@ -322,7 +310,7 @@ def main(args):
             epochs = args.iterations,
             batch_size = args.batch_size,
             seed = args.seed,
-            circuit_params_initialisation = qubit_init,
+            circuit_params_initialisation = circuit_init,
             mlp_params_initilaisation = mlp_init,
         )
     elif args.model == "alpha_4":
@@ -341,16 +329,65 @@ def main(args):
             epochs = args.iterations,
             batch_size = args.batch_size,
             seed = args.seed,
-            circuit_params_initialisation = qubit_init,
+            circuit_params_initialisation = circuit_init,
         )
     else:
         raise ValueError("An unrecognised model type was specified.")
 
+    # Train model and store model outputs
     
-    #Run pipeline
-    model.train()
-    model.compute_preds_prob_test()
-    #Store variables resulting from train and test and store in JSON
+    model.seed = args.seed
 
+    print(f'----------\n /
+           Beginning training for the set of parameters with ID: {args.exp_id}\n /
+           ----------\n')
+    t_before = time.time()
+    model.train()
+    t_after = time.time()
+    print('\n----- Training completed! -----\n')
+
+    train_loss_list = model.loss_train
+    train_preds_list = model.preds_train
+    train_probs_list = model.probs_train
+    val_loss_list =  model.loss_val
+    val_preds_list = model.preds_val
+    val_probs_list = model.probs_val
+
+    time_taken = t_after - t_before
+    print(f"Time taken to run ID {args.exp_id} = {time_taken}\n")
+
+    results_dict = {
+    'Params_ID': args.exp_id,
+    'Split': args.split,
+    'Seed': args.seed,
+    'Dataset': args.dataset.split('/')[-1],
+    'Epochs': args.epochs,
+    'Optimiser': args.optimiser,
+    'Batch Size': args.batch_size,
+    'Learning Rate': args.learning_rate,
+    'Embedder': args.embedder,
+    'Dim Reduction': args.dim_reduction,
+    'Ansatz': args.circuit,
+    'Num Qubits': args.n_qubits,
+    'Num Layers': args.n_layers,
+    'Data Rescaling': args.data_rescaling,
+    'Circuit Params Init': args.circuit_init,
+    'MLP Params Init': args.mlp_init,
+    'Train Loss': train_loss_list,
+    'Train Preds': train_preds_list,
+    'Train Probs': train_probs_list,
+    'Val Loss': val_loss_list,
+    'Val Preds': val_preds_list,
+    'Val Probs': val_probs_list,
+    'Runtime': time_taken
+}
+    
+    # Return model outputs
+        
+    return results_dict
+
+
+if __name__ == "__main__":
+    main()
 
     
