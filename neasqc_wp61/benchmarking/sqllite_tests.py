fe36f1a796872e235a73bd096b2b46a782f5962d
RW <richard.wolf@ichec.ie>
2024-04-24 10:46:36 +0100

    Test logic for the SQLlite queries

---
+++ neasqc_wp61/benchmarking/sqllite_tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+
+class TestDBQueries(unittest.TestCase):
+
+    def tear_down():
+
+    def test_table_creation_is_succesful():
+        # The table doesn't exist before I create it
+        # I create the table
+        # The table now exists
+        pass
+
+    def test_table_creation_has_all_wanted_rows():
+        # I create a table with a certain set of rows
+        # It contains all of those rows
+        # It doesn't contain any other rows
+        pass
+
+    def test_inserting_a_full_row_is_succesfull_and_correct():
+        # I insert data to populate a row
+        # All the specified rows are populated
+        # The values in each of them is correct
+        pass
+
+
+
