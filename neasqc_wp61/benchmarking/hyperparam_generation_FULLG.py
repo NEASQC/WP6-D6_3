22cb198824131e8016480188f6bb810e07d001b8
RW <richard.wolf@ichec.ie>
2024-04-25 12:29:46 +0100

    Remove unique IDs from HP generation

--- neasqc_wp61/benchmarking/hyperparam_generation_FULLG.py
+++ neasqc_wp61/benchmarking/hyperparam_generation_FULLG.py
@@ -14,12 +14,8 @@ mlp_init = ["unif", "norm"]
 
 combinations = list(itertools.product(model_types, embedding_type, dimensionality_reduction_technique, ansatz, number_of_layers, number_of_qubits, qubit_initialisation_technique, optimizer, optimizer_lr, mlp_init))
 
-unique_identifiers = [f"{i+1:05d}" for i in range(len(combinations))]
-
 df = pd.DataFrame(combinations, columns=['models', 'embeddings', 'dimensionality_reduction_technique', 'ansatz', 'nb_layers', 'nb_qubits', 'qubit_init_tecchnique', 'optimizer', 'optimizer_lr', 'mlp_init' ])
-
-df.insert(0, 'unique_id', unique_identifiers)
 
 df.to_csv('exp_hyperparameters_FULLG.tsv', sep='\t', index=False)
 
-#print(df)
+print(df)
