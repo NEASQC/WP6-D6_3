95464e39ee36a830ac0993653fbd922799bd85a4
pablo-lauret <pablo.lauret@yahoo.es>
2024-04-26 18:41:41 +0100

    Fixes typo RMSProp --> RMSprop

--- neasqc_wp61/benchmarking/hyperparam_generation_PRELIM.py
+++ neasqc_wp61/benchmarking/hyperparam_generation_PRELIM.py
@@ -8,14 +8,41 @@ ansatz = ["Sim14", "Sim15", "StronglyEntangling"]
 number_of_layers = [2]
 number_of_qubits = [8]
 qubit_initialisation_technique = ["norm", "rescaled_unif"]
-optimizer = ["Adam", "RMSProp"] 
-optimizer_lr = [0.001] 
+optimizer = ["Adam", "RMSprop"]
+optimizer_lr = [0.001]
 mlp_init = ["unif", "norm"]
 
-combinations = list(itertools.product(model_types, embedding_type, dimensionality_reduction_technique, ansatz, number_of_layers, number_of_qubits, qubit_initialisation_technique, optimizer, optimizer_lr, mlp_init))
+combinations = list(
+    itertools.product(
+        model_types,
+        embedding_type,
+        dimensionality_reduction_technique,
+        ansatz,
+        number_of_layers,
+        number_of_qubits,
+        qubit_initialisation_technique,
+        optimizer,
+        optimizer_lr,
+        mlp_init,
+    )
+)
 
-df = pd.DataFrame(combinations, columns=['models', 'embeddings', 'dimensionality_reduction_technique', 'ansatz', 'nb_layers', 'nb_qubits', 'qubit_init_tecchnique', 'optimizer', 'optimizer_lr', 'mlp_init' ])
+df = pd.DataFrame(
+    combinations,
+    columns=[
+        "models",
+        "embeddings",
+        "dimensionality_reduction_technique",
+        "ansatz",
+        "nb_layers",
+        "nb_qubits",
+        "qubit_init_tecchnique",
+        "optimizer",
+        "optimizer_lr",
+        "mlp_init",
+    ],
+)
 
-df.to_csv('exp_hyperparameters_PRELIM.tsv', sep='\t', index=False)
+df.to_csv("exp_hyperparameters_PRELIM.tsv", sep="\t", index=False)
 
 print(df)
